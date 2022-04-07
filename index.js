322e5df22da5394ea915da91110b649d76a7b3d6
Shawn Cannon <scanno16@ford.com>
2022-04-07 12:32:48 -0600

    change values

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const input = document.querySelector("#email-input")
 let output = document.querySelector("#email-output")
 
 button.addEventListener("click", function() {
-    if (input.value !== '') {
-        output.textContent = input.value
-    } else {
+    if (input.value === '') {
         output.textContent = "please insert valid email"
+    } else {
+        output.textContent = input.value
     }
 })
